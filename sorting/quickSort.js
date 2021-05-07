c956d6c785aa318ff5aea043f35a3fe2ff58327b
Gideon Nnalue <emekannalue@gmail.com>
2021-05-07 18:16:11 +0100

    solved quick sort

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -1,6 +1,36 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function quickSort(array) {}
+function quickSort(array, left, right) {
+  let pivot;
+  let partitionIndex;
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+}
+
+function partition(array, pivot, left, right) {
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex) {
+  const temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
 
 //Select first and last index as 2nd and 3rd parameters
 quickSort(numbers, 0, numbers.length - 1);
